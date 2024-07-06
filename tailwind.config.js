e62ba2f9089f0887aa55a688af4fc98200829c26
danial safari <30576163+danial-safari@users.noreply.github.com>
2024-07-06 18:40:59 +0330

    clear project , Add SFProDisplay font ,  Add Some Colors to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,32 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+
+const tailwindColor = {
+  transparent: 'transparent',
+  current: 'currentColor',
+  'white': '#ffffff',
+  'white-60': '#ffffff60',
+  'white-40': '#ffffff40',
+  'white-30': '#ffffff30',
+  'white-20': '#ffffff20',
+  'white-10': '#ffffff10',
+  'white-05': '#ffffff05',
+  'white-10': '#ffffff05',
+  'white-02': '#ffffff02',
+  black : '#000000',
+  'black-20' : '#00000020',
+  'black-60' : '#00000060',
+  'black-02' : '#00000002',
+  'bg-btn' : '#0B0B0B',
+  'icon-deactive' : '#6C6C6C'
+  
+}
+
+const config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,jsx}",
   ],
   theme: {
+    colors  : tailwindColor,
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
@@ -15,4 +36,5 @@ module.exports = {
     },
   },
   plugins: [],
-};
+}
+export default config;
